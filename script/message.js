0ac0e35694a372d96500f209518e3f33ac67d3a8
Olena <e2195610@cmaisonneuve.qc.ca>
2022-07-07 22:06:02 -0400

    Ajout de la fonctionnalité d’affiche de message dans la boîte modale

--- script/message.js
+++ script/message.js
@@ -27,19 +27,34 @@
         elmBoite__modale.classList.add('boite__modale__ouvrir') 
         elmCover.classList.add('dark')    
         console.log(elmBoite__modale.classList)
+        afficherMessage();
     })
 
     elmBoite__X.addEventListener('mousedown', function(){
         elmBoite__modale.classList.remove('boite__modale__ouvrir');        
-        console.log(elmBoite__modale.classList)
-        elmCover.classList.remove('dark') 
+        console.log(elmBoite__modale.classList);
+        elmCover.classList.remove('dark');
+        conteneurMessage.innerHTML = ''
+        elmBoite__modale.removeChild(conteneurMessage)
+        
     })
 
-    let sectionArticle
-    let titre
-    sectionArticle = document.querySelector('.site__main')
-    console.log(sectionArticle)
-    titre = sectionArticle.querySelector('h2').innerHTML
-    console.log(titre) 
+    function afficherMessage(){
+        let sectionArticle
+        let titre
+        sectionArticle = document.querySelector('.site__main')
+        //console.log(sectionArticle)
+        titre = sectionArticle.querySelector('h2').innerHTML
+        //console.log(titre) 
+        conteneurMessage = document.createElement('div')
+        elmBoite__modale.appendChild(conteneurMessage)
+        conteneurMessage.classList.add('conteneur__message')
+        //console.log(conteneurMessage)
+        let message = `
+                        L'inscription au cours ${titre} est déjà ouverte!
+                      `
+        conteneurMessage.insertAdjacentHTML('afterbegin', message)
+
+    }
 
 })()
